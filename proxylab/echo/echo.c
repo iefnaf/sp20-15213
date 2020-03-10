3b0b7574e42043add2dd08dbdbc4f785c361520c
gaofanfei <gfanfei@gmail.com>
2020-03-10 22:07:20 +0800

    proxylab init

---
+++ proxylab/echo/echo.c
@@ -0,0 +1,15 @@
+#include "csapp.h"
+
+void echo(int connfd)
+{
+    size_t n;
+    char buf[MAXLINE];
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd);
+    while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0)
+    {
+        printf("server received %d bytes\n", (int)n);
+        Rio_writen(connfd, buf, n);
+    }
+}
