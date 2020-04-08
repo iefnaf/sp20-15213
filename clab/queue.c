265edbe2f3da5e86e0cb56699577295ccab3592c
gaofanfei <gfanfei@gmail.com>
2020-04-08 02:03:57 -0700

    lab0:cprogramming lab

--- clab/queue.c
+++ clab/queue.c
@@ -24,9 +24,15 @@
 */
 queue_t *q_new()
 {
-    queue_t *q =  malloc(sizeof(queue_t));
+    queue_t *q =  (queue_t*)malloc(sizeof(queue_t));
+    if (q == NULL)
+    {
+      return NULL;
+    }
     /* What if malloc returned NULL? */
     q->head = NULL;
+    q->tail = NULL;
+    q->_size = 0;
     return q;
 }
 
@@ -35,6 +41,22 @@ void q_free(queue_t *q)
 {
     /* How about freeing the list elements and the strings? */
     /* Free queue structure */
+    if (q == NULL)
+    {
+      return;
+    }
+    for (int i = 0; i < q->_size; i++)
+    {
+      list_ele_t *p = q->head;
+      q->head = q->head->next;
+      if (p->value != NULL)
+      {
+        free(p->value);
+      }
+      
+      free(p);
+    }
+
     free(q);
 }
 
@@ -47,13 +69,39 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
-    list_ele_t *newh;
-    /* What should you do if the q is NULL? */
-    newh = malloc(sizeof(list_ele_t));
+    /* What should you do if the q is NULL? return false*/
+    if (q == NULL)
+    {
+      return false;
+    }
+
+    list_ele_t *newh = (list_ele_t*)malloc(sizeof(list_ele_t));
+    if (newh == NULL)
+    {
+      return false;
+    }
     /* Don't forget to allocate space for the string and copy it */
+    newh->value = (char*)malloc(strlen(s) + 1);
     /* What if either call to malloc returns NULL? */
-    newh->next = q->head;
-    q->head = newh;
+    if (newh->value == NULL)
+    {
+      free(newh); //don't forget to free
+      return false;
+    }
+
+    strcpy(newh->value, s);
+    //当前队列为空
+    if (q->_size == 0)
+    {
+      q->head = newh;
+      q->tail = newh;
+    }
+    else
+    {
+      newh->next = q->head;
+      q->head = newh;
+    }
+    q->_size++;
     return true;
 }
 
@@ -69,7 +117,38 @@ bool q_insert_tail(queue_t *q, char *s)
 {
     /* You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
-    return false;
+    if (q == NULL)
+    {
+      return false;
+    }
+
+    list_ele_t *newt = (list_ele_t*)malloc(sizeof(list_ele_t));
+    if (newt == NULL)
+    {
+      return false;
+    }
+    newt->value = (char*)malloc(strlen(s) + 1);
+    if (newt->value == NULL)
+    {
+      free(newt);
+      return false;
+    }
+
+    strcpy(newt->value, s);
+
+    if (q->tail == NULL)//当前队列中元素为空
+    {
+      q->head = newt;
+      q->tail = newt;
+    }
+    else
+    {
+      q->tail->next = newt;
+      q->tail = newt;
+    }
+    newt->next = NULL;
+    q->_size++;
+    return true;
 }
 
 /*
@@ -83,7 +162,28 @@ bool q_insert_tail(queue_t *q, char *s)
 bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
     /* You need to fix up this code. */
+    if (q == NULL || q->_size == 0)
+    {
+      return false;
+    }
+
+    list_ele_t *oldh = q->head;
     q->head = q->head->next;
+    oldh->next = NULL;
+
+    if (sp != NULL)
+    {
+      strncpy(sp, oldh->value, bufsize - 1);
+      sp[bufsize-1] = '\0'; 
+    }
+    
+    if (oldh->value != NULL)
+    {
+      free(oldh->value);
+    }
+    
+    free(oldh);
+    q->_size--;
     return true;
 }
 
@@ -95,7 +195,11 @@ int q_size(queue_t *q)
 {
     /* You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
-    return 0;
+    if (q == NULL)
+    {
+      return 0;
+    }
+    return q->_size;
 }
 
 /*
@@ -107,6 +211,21 @@ int q_size(queue_t *q)
  */
 void q_reverse(queue_t *q)
 {
-    /* You need to write the code for this function */
+    if (q == NULL || q->_size == 0)
+    {
+      return;
+    }
+
+    list_ele_t *oldh = q->head;
+    list_ele_t *oldt = q->tail;
+    for (int i = 0; i < q->_size - 1; i++)
+    {
+      list_ele_t *h = q->head;
+      q->head = q->head->next;
+      h->next = q->tail->next;
+      q->tail->next = h;
+    }
+    q->head = oldt;
+    q->tail = oldh;
 }
 
--- clab/queue.h
+++ clab/queue.h
@@ -26,6 +26,8 @@ typedef struct ELE {
 /* Queue structure */
 typedef struct {
     list_ele_t *head;  /* Linked list of elements */
+    list_ele_t *tail;
+    int _size;
     /*
       You will need to add more fields to this structure
       to efficiently implement q_size and q_insert_tail
