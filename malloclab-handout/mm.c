0700a6d9b98588f8affa81e782bdc4e2279106a5
gaofanfei <gfanfei@gmail.com>
2020-06-14 23:07:51 -0700

    add trace files for malloc lab

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -24,11 +24,11 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "-----BaseLine-----",
     /* First member's full name */
-    "Harry Bovik",
+    "gaofanfei",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+    "gaofanfei2020@163.com",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
